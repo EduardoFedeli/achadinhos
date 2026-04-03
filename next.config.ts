805eead3f9016e2d87af826b49071b31d7c2a01f
Eduardo Fedeli <dudufedeli@outlook.com>
2026-04-02 23:21:30 -0300

    Troca de senha da API

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      // Caso futuramente você use links da Amazon ou AliExpress, 
+      // precisaremos adicionar os domínios deles aqui também.
+    ],
+  },
 };
 
 export default nextConfig;
